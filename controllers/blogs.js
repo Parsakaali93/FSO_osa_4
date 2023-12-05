1cda22e7ba5d4b3c5930362e7e8fd5444da5b7cb
Parsakaali <maukissamau@gmail.com>
2023-12-05 23:56:35 +0200

    exercise 4.14 done except test

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,15 +3,32 @@ const Blog = require('../models/blog')
 
 // ROUTE TO GET ALL BLOGS
 blogsRouter.get('/', (request, response) => {
+
+  console.log("GET ALL")
+
     Blog
       .find({})
       .then(blogs => {
         response.json(blogs)
       })
   })
-  
+
+  // ROUTE TO GET 1 BLOG
+  blogsRouter.get('/:id', async (request, response) => {
+    console.log("GET 1 BLOG")
+
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+  })
+
   // ROUTE TO POST A BLOG
   blogsRouter.post('/', (request, response) => {
+    console.log("POST A NEW BLOG")
+
     let blogData = request.body
 
     if(!blogData.hasOwnProperty('likes')){
@@ -26,5 +43,44 @@ blogsRouter.get('/', (request, response) => {
         response.status(201).json(result)
       })
   })
+
+    // ROUTE TO DELETE A BLOG
+    blogsRouter.delete('/:id', async (request, response) => {
+      console.log("DELETE A BLOG")
+
+        await Blog.findByIdAndDelete(request.params.id)
+        response.status(204).end()
+    })
+
+
+    // ROUTE TO UPDATE A BLOG
+    blogsRouter.put('/:id', async (request, response) => {
+      try{
+        console.log(request.params.id)
+
+      let blogData = request.body
+
+      const blog = {
+        title: blogData.title,
+        author: blogData.author,
+        url: blogData.url,
+        likes: blogData.likes
+      }
+
+      let updatedBlog = await Blog.findOneAndUpdate({_id: request.params.id}, blog, {new:true})
+
+      if (!updatedBlog) {
+        return response.status(404).json({ error: 'Blog not found' });
+      }
+
+      response.json(updatedBlog)
+
+      }
+
+      catch(error){
+        console.error(error)
+        response.status(500).json({ error: 'Internal server error' })
+      }
+    })
 
   module.exports = blogsRouter
--- tests/tests.test.js
+++ tests/tests.test.js
@@ -104,7 +104,28 @@ describe('total likes', () => {
         'Go To Statement Considered Harmful'
       )
   })
+
+  test('a blog can be deleted', async() => {
+
+    const newBlog = {
+        id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+      }
   
+      await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+      await api
+      .delete('/api/blogs/5a422aa71b54a676234d17f8')
+      .expect(204)
+  })
 
   afterAll(async () => {
     await mongoose.connection.close()
